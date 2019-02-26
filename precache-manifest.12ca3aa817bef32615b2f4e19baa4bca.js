be03b2df6ddd3401d826a1b36ce8cfce7baeccf7
Emmanuel Eze <emereuwaonueze@gmail.com>
2019-02-26 17:40:04 +0100

    Updates

---
+++ precache-manifest.12ca3aa817bef32615b2f4e19baa4bca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "aedb4e53e89ac656e8a6",
+    "url": "/robofriends/static/js/runtime~main.aedb4e53.js"
+  },
+  {
+    "revision": "02236120440e77fa44cd",
+    "url": "/robofriends/static/js/main.02236120.chunk.js"
+  },
+  {
+    "revision": "39cef601ce8c40471500",
+    "url": "/robofriends/static/js/2.39cef601.chunk.js"
+  },
+  {
+    "revision": "02236120440e77fa44cd",
+    "url": "/robofriends/static/css/main.3326b2fa.chunk.css"
+  },
+  {
+    "revision": "39cef601ce8c40471500",
+    "url": "/robofriends/static/css/2.fb9286e0.chunk.css"
+  },
+  {
+    "revision": "d2f96269bffe8097e35fb9fb0297fc86",
+    "url": "/robofriends/index.html"
+  }
+];
