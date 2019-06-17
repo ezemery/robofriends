8131f83dcc1f75172ee504cff517181f9e53259b
Emmanuel Eze <emereuwaonueze@gmail.com>
2019-06-17 17:05:41 +0100

    Add: circleci configuration

--- src/index.js
+++ src/index.js
@@ -19,3 +19,24 @@ ReactDOM.render(<Provider store={store}>
                 </Provider>,
                 document.getElementById('root'));
 registerServiceWorker();
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import './index.css';
+import App from './containers/App'; 
+import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+import { searchRobots, requestRobots } from './reducers';
+
+const logger = createLogger();
+const rootReducer = combineReducers({searchRobots, requestRobots });
+const  store = createStore(rootReducer, applyMiddleware(thunk,logger));
+
+ReactDOM.render(<Provider store={store}>
+                    <App />
+                </Provider>,
+                document.getElementById('root'));
+registerServiceWorker();
